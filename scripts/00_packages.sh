151adc36280a721fc07771d595dc9508b73fe63e
ctrlaltreboot <rj@rebooter.xyz>
2018-04-29 13:10:40 +1000

    Add more packages

--- scripts/00_packages.sh
+++ scripts/00_packages.sh
@@ -5,4 +5,5 @@ silversearcher-ag cowsay cowsay-off \
 fortunes fortune-mod fortunes-off \
 strongswan-nm strongswan \
 redshift redshift-gtk \
-i3 fonts-powerline
+i3 fonts-powerline i3status suckless-toos \
+i3lock xbacklight feh conky
