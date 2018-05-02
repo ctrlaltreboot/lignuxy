04f19e60acf40554dd047a24da82c8ef28ea51b9
ctrlaltreboot <rj@rebooter.xyz>
2018-05-02 23:03:06 +1000

    Update packages installation script to support Arch Linux

--- scripts/00_packages.sh
+++ scripts/00_packages.sh
@@ -1,48 +1,35 @@
 #!/usr/bin/env bash
 
-arch_pkd() {
-	sudo pacman -Syu
-	sudo pacman -S --noconfirm --needed vim \
-	openssh \
-	zsh \
-	tmux \
-	terminator \
-	autojump
-	the_silver_searcher \
-	networkmanager-strongswan strongswan \
-        vim cowsay cowsay-off \
-	redshift redshift-gtk \
-	fortunes fortune-mod fortunes-off \
-	fonts-powerline i3status suckless-tools \
-	i3lock xbacklight feh conky lxappearance \
-	scrot unclutter \
+source .helpers
+
+pac_pkg() {
+    sudo pacman -Syu
+    sudo pacman -S --noconfirm --needed \
+    vim openssh zsh \
+    tmux terminator \
+    autojump the_silver_searcher \
+    networkmanager-strongswan strongswan \
+    cowsay cowfortune fortune-mod \
+    powerline-fonts i3status \
+    i3lock feh conky scrot unclutter \
+    archey3 baobab bleachbit catfish clementine \
+    conky curl redshift python python-pip git yaourt \
+    docker lsof
+    }
+
+apt_pkg() {
+    sudo apt-get update -y
+    sudo apt-get install -y \
+    vim ssh zsh \
+    tmux terminator autojump  \
+    silversearcher-ag cowsay cowsay-off \
+    fortunes fortune-mod fortunes-off \
+    strongswan-nm strongswan \
+    redshift redshift-gtk \
+    i3 fonts-powerline i3status suckless-tools \
+    i3lock xbacklight feh conky lxappearance \
+    scrot compton unclutter \
+    pasystray paman paprefs pavumeter pulseaudio-module-zeroconf
 }
 
-deb_pkg() {
-	sudo apt-get update -y
-	sudo apt-get install -y \
-	vim ssh zsh \
-	tmux terminator autojump  \
-	silversearcher-ag cowsay cowsay-off \
-	fortunes fortune-mod fortunes-off \
-	strongswan-nm strongswan \
-	redshift redshift-gtk \
-	i3 fonts-powerline i3status suckless-tools \
-	i3lock xbacklight feh conky lxappearance \
-	scrot compton unclutter \
-	pasystray paman paprefs pavumeter pulseaudio-module-zeroconf
-}
-
-wat() {
-	which apt-get &> /dev/null
-        APT=$?
-        which pacman &> /dev/null
-        PAC=$?
-	if (( $APT < $PAC )); then
-		deb_pkg
-	elif (( $PAC < $APT )); then
-		apt_pkg
-	fi
-}
-
-wat
+arch_or_deb pac_pkg apt_pkg
